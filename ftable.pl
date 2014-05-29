d748ee487e90bc0e67c32da49568f03f752cf584
tiago <tiago@dell.(none)>
2014-05-29 18:56:56 +0100

    Added License

--- ftable.pl
+++ ftable.pl
@@ -18,6 +18,7 @@ our $minus="-";
 our $FS=',';
 our $nb=0;
 our $ho=0;
+our $license=0;
 
 my %h;
 if ($#ARGV >= 0){
@@ -30,8 +31,10 @@ if ($#ARGV >= 0){
            		'F:s'       	   => \$FS,
            		'n|noborder'       => \$nb,
            		'h|header'       => \$ho,
+           		'license'       => \$license,
           	) || print_usage();
 
+	$license && print_license();
 	%h=get_details($lf,$cf,$rf,$print);
 }else {
 	%h=get_details();
@@ -420,3 +423,31 @@ EOF
 	print $usage;
 	exit 2;
 }
+
+sub print_license {
+my $license= << 'EOF';
+Copyright (c) 2013 Tiago Lopo Da Silva.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+EOF
+	print $license; 
+	exit; 
+}
+
